6ff7004e945f8f4097fc72a2181e6dc6aca52b62
Filip1139 <filip1139@gmail.com>
2026-01-13 11:00:23 +0100

    style: refactor header component class names for consistency and update related CSS styles

--- src/components/sections/HeadersSection.vue
+++ src/components/sections/HeadersSection.vue
@@ -18,29 +18,29 @@ const emit = defineEmits<{
       <DocsExample
         @copy="emit('copyHtml')"
         :code="`<div class='c-header'>
-  <div class='c-header__icon'>
+  <div class='c-header-icon'>
     <div class='c-featured-icon c-featured-icon--brand c-featured-icon--l'>
       <i class='ph ph-puzzle-piece'></i>
     </div>
   </div>
-  <div class='c-header__content'>
-    <h2 class='c-header__title t-h2 t-font-medium'>Przyporządkuj opisy badań do nazw nauk przyrodniczych</h2>
+  <div class='c-header-content'>
+    <h2 class='c-header-title t-h2 t-font-medium'>Przyporządkuj opisy badań do nazw nauk przyrodniczych</h2>
   </div>
-  <button type='button' class='c-header__close c-btn c-btn-icon c-btn-md c-btn-tertiary-ghost' data-modal-close>
+  <button type='button' class='c-header-close c-btn c-btn-icon c-btn-md c-btn-tertiary-ghost' data-modal-close>
     <i class='ph-bold ph-x'></i>
   </button>
 </div>`"
       >
         <div class="c-header" style="background: white; border-radius: 1rem;">
-          <div class="c-header__icon">
+          <div class="c-header-icon">
             <div class="c-featured-icon c-featured-icon--brand c-featured-icon--l">
               <i class="ph ph-puzzle-piece"></i>
             </div>
           </div>
-          <div class="c-header__content">
-            <h2 class="c-header__title t-h2 t-font-medium">Przyporządkuj opisy badań do nazw nauk przyrodniczych</h2>
+          <div class="c-header-content">
+            <h2 class="c-header-title t-h2 t-font-medium">Przyporządkuj opisy badań do nazw nauk przyrodniczych</h2>
           </div>
-          <button type="button" class="c-header__close c-btn c-btn-icon c-btn-md c-btn-tertiary-ghost" data-modal-close>
+          <button type="button" class="c-header-close c-btn c-btn-icon c-btn-md c-btn-tertiary-ghost" data-modal-close>
             <i class="ph-bold ph-x"></i>
           </button>
         </div>
